9a8a41cdfdabfd2b00373597d244ab8c85bb0613
HT-35 <huyfa352002@gmail.com>
2024-04-23 00:17:44 +0700

    save

--- assets/js/FE/admin-dashboard.FE.js
+++ assets/js/FE/admin-dashboard.FE.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+import { callApiMethodGet } from "../utils/callApi.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
   const insertInfo = document.querySelector("#insert-info");
   const token = localStorage.getItem("accessToken");
   //console.log(token);
@@ -20,4 +22,68 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     window.location.href = "./login.html";
   }
+
+  // ==================   Handle signout =============================
+
+  const btnSignout = document.querySelector(".btn-signout");
+  btnSignout.addEventListener("click", () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("userName");
+    window.location.href = "/index.html";
+  });
+
+  // ==========================  Get Detail Accout   =================================
+  const user = await callApiMethodGet(
+    "http://localhost:3000/user/info-user",
+    localStorage.getItem("accessToken")
+  );
+  const { email, fullName, numberPhone } = user.data;
+
+  const insertData = document.querySelector(".insert-data");
+
+  const templateInfo = `
+  
+                                                      <div class="">
+                                                        <div class="row text-center">
+                                                            <div class="col-1 col-sm-1 col-md-1"></div>
+                                                            <div class="col-3 col-sm-3 col-md-3">
+                                                                Họ Tên :
+                                                            </div>
+                                                            <div class="col-8 col-sm-8 col-md-8 text-left">
+                                                               ${fullName}
+                                                            </div>
+                                                        </div>
+                                                        <div class="row text-center">
+                                                            <div class="col-1 col-sm-1 col-md-1"></div>
+                                                            <div class="col-3 col-sm-3 col-md-3">
+                                                                Email :
+                                                            </div>
+                                                            <div class="col-8 col-sm-8 col-md-8 text-left">
+                                                               ${email}
+                                                            </div>
+                                                        </div>
+                                                        <div class="row text-center">
+                                                            <div class="col-0 col-sm-0 col-md-1"></div>
+                                                            <div class="col-4 col-sm-4 col-md-3">
+                                                                Số Điện Thoại :
+                                                            </div>
+                                                            <div class="col-8 col-sm-8 col-md-8 text-left">
+                                                               ${numberPhone}
+                                                            </div>
+                                                        </div>
+                                                       <div class="row text-center">
+                                                            <div class="col-0 col-sm-0 col-md-1"></div>
+                                                            <div class="col-4 col-sm-4 col-md-3">
+                                                               Vai trò:
+                                                            </div>
+                                                            <div class="col-8 col-sm-8 col-md-8 text-left">
+                                                              Admin
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+  `;
+  insertData.insertAdjacentHTML("beforeend", templateInfo);
+
+  // ==========================  Get NEW bILL   =================================
 });
---
+++ assets/js/FE/admin-login.FE.js
@@ -0,0 +1,88 @@
+import { fetchMethodPost } from "../utils/callApi.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // ========================  REGISTER ========================
+
+  // ========================  Login ========================
+  const formSingin = document.querySelector("#singin-form");
+
+  formSingin.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const notifi = document.querySelector("#notifi");
+    const buttonSingin = document.querySelector("#button-singin");
+
+    const singinEmail = document.querySelector("#singin-email");
+    const singinPassword = document.querySelector("#singin-password");
+    console.log(singinEmail, singinPassword);
+
+    singinEmail.addEventListener("change", () => {
+      buttonSingin.removeAttribute("disabled");
+      notifi.innerHTML = "";
+    });
+    singinPassword.addEventListener("change", () => {
+      buttonSingin.removeAttribute("disabled");
+      notifi.innerHTML = "";
+    });
+
+    const account = {
+      username: singinEmail.value.trim(),
+      password: singinPassword.value.trim(),
+    };
+    console.log("account:", account);
+
+    async function login(data) {
+      const postData = await fetchMethodPost(
+        "http://localhost:3000/login/admin",
+        data
+      );
+      const res = await postData;
+      console.log("res:", res);
+      if (res.status === true) {
+        alert("login successfull");
+        console.log(res);
+        const { token, fullName } = res;
+        console.log({ token, fullName });
+        showProfile(token, fullName);
+        window.location.href = "http://127.0.0.1:5502/admin-dashboad.html";
+      } else {
+        if ((res.data = "user does not have permission !!!")) {
+          const template = `<span class="text-danger">Bạn Không Có Quyền Truy Cập !!!</span>`;
+          notifi.insertAdjacentHTML("beforeend", template);
+          buttonSingin.setAttribute("disabled", "");
+        } else {
+          const template = `<span class="text-danger">Tài Khoản Hoặc Mật Khẩu Sai !!</span>`;
+          notifi.insertAdjacentHTML("beforeend", template);
+          buttonSingin.setAttribute("disabled", "");
+        }
+      }
+    }
+    login(account);
+  });
+});
+
+async function createUser(data) {
+  const postData = await fetchMethodPost(
+    "http://localhost:3000/user/create",
+    data
+  );
+  const res = await postData;
+  console.log(res);
+  if (res.status === true) {
+    alert("Tạo Tài Khoản Thành Công !! ");
+    window.location.href = "http://127.0.0.1:5502/login.html";
+  } else {
+    const notifi = `${res.data.errors[0].path} đã tồn tại`;
+    alert(notifi);
+  }
+}
+
+function isValidPhoneNumber(phone) {
+  // Sử dụng regular expression để kiểm tra số điện thoại có đúng định dạng không (ví dụ: có đủ 10 số)
+  const phoneRegex = /^\d{10}$/;
+  return phoneRegex.test(phone);
+}
+
+function showProfile(token, username) {
+  localStorage.setItem("accessToken", token);
+  localStorage.setItem("userName", username);
+}
